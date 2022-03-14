c5ea2fd6deaae07dfceb704e09309d2cff030a21
menvdamm <80266975+menvdamm@users.noreply.github.com>
2022-03-14 20:26:23 +0100

    Example Tree
    Implemented simple example tree

--- lib/widgets/Tree.dart
+++ lib/widgets/Tree.dart
@@ -1,8 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:graphview/graphview.dart';
 import 'dart:math';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:graphview/graphview.dart';
+var json = {
+  "nodes": [
+    {"id": 1, "label": 'circle'},
+    {"id": 2, "label": 'ellipse'},
+    {"id": 3, "label": 'database'},
+    {"id": 4, "label": 'box'},
+    {"id": 5, "label": 'diamond'},
+    {"id": 6, "label": 'dot'},
+    {"id": 7, "label": 'square'},
+    {"id": 8, "label": 'triangle'},
+  ],
+  "edges": [
+    {"from": 1, "to": 2},
+    {"from": 2, "to": 3},
+    {"from": 2, "to": 4},
+    {"from": 2, "to": 5},
+    {"from": 5, "to": 6},
+    {"from": 5, "to": 7},
+    {"from": 6, "to": 8}
+  ]
+};
 
 class TreeViewPage extends StatefulWidget {
   const TreeViewPage({Key? key}) : super(key: key);
@@ -12,72 +33,13 @@ class TreeViewPage extends StatefulWidget {
 }
 
 class _TreeViewPageState extends State<TreeViewPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
           mainAxisSize: MainAxisSize.max,
           children: [
-            Wrap(
-              children: [
-                SizedBox(
-                  width: 100,
-                  child: TextFormField(
-                    initialValue: builder.siblingSeparation.toString(),
-                    decoration: const InputDecoration(labelText: "Sibling Separation"),
-                    onChanged: (text) {
-                      builder.siblingSeparation = int.tryParse(text) ?? 100;
-                      setState(() {});
-                    },
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: TextFormField(
-                    initialValue: builder.levelSeparation.toString(),
-                    decoration: const InputDecoration(labelText: "Level Separation"),
-                    onChanged: (text) {
-                      builder.levelSeparation = int.tryParse(text) ?? 100;
-                      setState(() {});
-                    },
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: TextFormField(
-                    initialValue: builder.subtreeSeparation.toString(),
-                    decoration: const InputDecoration(labelText: "Subtree separation"),
-                    onChanged: (text) {
-                      builder.subtreeSeparation = int.tryParse(text) ?? 100;
-                      setState(() {});
-                    },
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: TextFormField(
-                    initialValue: builder.orientation.toString(),
-                    decoration: const InputDecoration(labelText: "Orientation"),
-                    onChanged: (text) {
-                      builder.orientation = int.tryParse(text) ?? 100;
-                      setState(() {});
-                    },
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    final node12 = Node.Id(r.nextInt(100));
-                    var edge = graph.getNodeAtPosition(r.nextInt(graph.nodeCount()));
-                    if (kDebugMode) {
-                      print(edge);
-                    }
-                    graph.addEdge(edge, node12);
-                    setState(() {});
-                  },
-                  child: const Text("Add"),
-                )
-              ],
-            ),
             Expanded(
               child: InteractiveViewer(
                   constrained: false,
@@ -88,13 +50,15 @@ class _TreeViewPageState extends State<TreeViewPage> {
                     graph: graph,
                     algorithm: BuchheimWalkerAlgorithm(builder, TreeEdgeRenderer(builder)),
                     paint: Paint()
-                      ..color = Colors.green
+                      ..color = Colors.black
                       ..strokeWidth = 1
                       ..style = PaintingStyle.stroke,
                     builder: (Node node) {
                       // I can decide what widget should be shown here based on the id
-                      var a = node.key?.value as int;
-                      return rectangleWidget(a);
+                      var a = node.key!.value as int;
+                      var nodes = json['nodes'];
+                      var nodeValue = nodes!.firstWhere((element) => element['id'] == a);
+                      return rectangleWidget(nodeValue['label'] as String);
                     },
                   )),
             ),
@@ -104,20 +68,22 @@ class _TreeViewPageState extends State<TreeViewPage> {
 
   Random r = Random();
 
-  Widget rectangleWidget(int a) {
+  Widget rectangleWidget(a) {
     return InkWell(
       onTap: () {
-        print('clicked');
+          if (kDebugMode) {
+            print('clicked');
+          }
       },
       child: Container(
           padding: const EdgeInsets.all(16),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(4),
-            boxShadow: [
-              const BoxShadow(color: Colors.blue, spreadRadius: 1),
+            boxShadow: const [
+              BoxShadow(color: Colors.blue, spreadRadius: 1),
             ],
           ),
-          child: Text('Node ${a}')),
+          child: Text('Node $a')),
     );
   }
 
@@ -126,35 +92,16 @@ class _TreeViewPageState extends State<TreeViewPage> {
 
   @override
   void initState() {
-    final node1 = Node.Id(1);
-    final node2 = Node.Id(2);
-    final node3 = Node.Id(3);
-    final node4 = Node.Id(4);
-    final node5 = Node.Id(5);
-    final node6 = Node.Id(6);
-    final node8 = Node.Id(7);
-    final node7 = Node.Id(8);
-    final node9 = Node.Id(9);
-    final node10 = Node.Id(10);
-    final node11 = Node.Id(11);
-    final node12 = Node.Id(12);
-
-    graph.addEdge(node1, node2);
-    graph.addEdge(node1, node3, paint: Paint()..color = Colors.red);
-    graph.addEdge(node1, node4, paint: Paint()..color = Colors.blue);
-    graph.addEdge(node2, node5);
-    graph.addEdge(node2, node6);
-    graph.addEdge(node6, node7, paint: Paint()..color = Colors.red);
-    graph.addEdge(node6, node8, paint: Paint()..color = Colors.red);
-    graph.addEdge(node4, node9);
-    graph.addEdge(node4, node10, paint: Paint()..color = Colors.black);
-    graph.addEdge(node4, node11, paint: Paint()..color = Colors.red);
-    graph.addEdge(node11, node12);
+    for (var element in json['edges']!) {
+      var fromNodeId = element['from'];
+      var toNodeId = element['to'];
+      graph.addEdge(Node.Id(fromNodeId), Node.Id(toNodeId));
+    }
 
     builder
       ..siblingSeparation = (100)
       ..levelSeparation = (150)
       ..subtreeSeparation = (150)
-      ..orientation = (BuchheimWalkerConfiguration.ORIENTATION_TOP_BOTTOM);
+      ..orientation = (BuchheimWalkerConfiguration.ORIENTATION_LEFT_RIGHT);
   }
 }
